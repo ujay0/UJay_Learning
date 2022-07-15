864738f0b9beba77a155ac1201f54755779a82d0
ujay0 <uutkarsh2@yahoo.com>
2022-07-15 14:10:58 +0530

    Mutable Python Datatypes & Mutability vs Immutable
    object,
    list,
    tuple,
    set,
    frozenset,
    dict,
    range,
    bytearray,
    bytes,
    none.

---
+++ YT Python Full Fundamental Course for Beginners by Durga Sir/V033-PDataType_list.py
@@ -0,0 +1,10 @@
+l=[10,'durga',20,10,30]
+print('Type l:',type(l))
+print('LIST l:',l)
+
+# Used to store Group of Values as a Single entity, where order is Important & Duplicates are allowed we use List Data Type.
+
+
+print('LIST l[0]:',l[0])
+print('LIST l[-1]:',l[-1])
+print('LIST l[1:4]:',l[1:4])	#List Slicing from index 1 to 4 i.e (4-1) = 3 elements
