5777122b7b8de0517ea81cefafc8f4e77deee819
UJay  <uutkarsh2@yahoo.com>
2026-03-18 02:31:18 +0530

    File name correction with the problem statment for

---
+++ HR_Solutions/Tut30Days/D2DataType.py
@@ -0,0 +1,27 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j = 0
+e=0.0
+t=''
+# Read and save an integer, double, and String to your variables.
+j =input("Please enter Integer")
+e=input("Please enter Double")
+t=input("Please enter String")
+# print(inputArr)
+# j = int(inputArr[0])
+# e = float(inputArr[1])
+# t = inputArr[3]
+
+
+# Print the sum of both integer variables on a new line.
+print(i+(int(j)))
+
+# Print the sum of the double variables on a new line.
+print(d+
+    float(e))
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+t)
