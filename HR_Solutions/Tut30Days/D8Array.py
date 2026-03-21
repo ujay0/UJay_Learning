f4f39aca6bacc097560daa3c7e9845d62f3ba0e8
UJay  <uutkarsh2@yahoo.com>
2026-03-21 12:19:23 +0530

    Updating the Solution without for each

--- HR_Solutions/Tut30Days/D8Array.py
+++ HR_Solutions/Tut30Days/D8Array.py
@@ -12,5 +12,6 @@ if __name__ == '__main__':
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
-    for i in range(len(arr)-1, -1, -1):
-        print(arr[i], end=' ')
+    # for i in range(len(arr)-1, -1, -1):
+        # print(arr[i], end=' ')
+    print(" ".join(map(str, arr[::-1])))
