e295393ebc4f33a4dee43f317581a485318423d6
UJay  <uutkarsh2@yahoo.com>
2026-03-21 12:35:45 +0530

    Writing recursive factorial function

---
+++ HR_Solutions/Tut30Days/D10Recurtion.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+"""Complete the factorial function in the editor below. Be sure to use recursion. 
+factorial has the following paramter: int n: an integer
+Returns int: the factorial of n
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of 0."""
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'factorial' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def factorial(n):
+    # Write your code here
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
