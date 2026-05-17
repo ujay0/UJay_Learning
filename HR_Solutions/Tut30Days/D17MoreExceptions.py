3bc1c3a98f5543a92578eeb5a8ce4c981fd67c3a
UJay  <uutkarsh2@yahoo.com>
2026-05-17 22:31:20 +0530

    Learning challenge to practice throwing and propagating an exception

---
+++ HR_Solutions/Tut30Days/D17MoreExceptions.py
@@ -0,0 +1,35 @@
+# Task
+# Write a Calculator class with a single method: int power(int,int). 
+# The power method takes two integers, n and p, as parameters and returns 
+# the integer result of n^p. If either n or p is negative, then the method must 
+# throw an exception with the message: n and p should be non-negative.
+
+# Note: Do not use an access modifier (e.g.: public) in the declaration 
+# for your Calculator class.
+
+# Input Format
+
+# Input from stdin is handled for you by the locked stub code in your editor. 
+# The first line contains an integer,T, the number of test cases. Each of the T 
+# subsequent lines describes a test case in 2 space-separated integers that denote  
+#  n and p respectively.
+
+
+#Write your code here
+class Calculator:
+    def power(self, n, p):
+        if n <0 or p <0:
+            raise Exception("n & p should both be non-negative real numbers.")
+        else:
+            return n**p
+    
+    
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
