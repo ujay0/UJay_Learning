fe481af4f0830ae332c8afca1009acf158306351
UJay  <uutkarsh2@yahoo.com>
2026-05-22 01:57:32 +0530

    Add D26NestedLogic.py to calculate library book fines based on return dates

---
+++ HR_Solutions/Tut30Days/D26NestedLogic.py
@@ -0,0 +1,13 @@
+# Your local library needs your help! Given the expected and actual return dates for a 
+# library book, create a program that calculates the fine (if any). The fee structure 
+# is as follows:
+# If the book is returned on or before the expected return date, no fine will be charged 
+# (i.e.:fine =0 ).
+#  If the book is returned after the expected return day but still within the 
+# same calendar month and year as the expected return date, fine = 15 * (number of days late).
+# If the book is returned after the expected return month but still within the same 
+# calendar year as the expected return date, the fine = 500 * (number of months late).
+# If the book is returned after the calendar year in which it was expected, there is a fixed fine of 10000.
+
+
+
