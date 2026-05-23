40c9ef4ae9c5f03ca021dd344abce78829d224fb
UJay  <uutkarsh2@yahoo.com>
2026-05-23 22:41:08 +0530

    Implement test data classes and complete minimum index function for array validation

--- HR_Solutions/Tut30Days/D27Testing.py
+++ HR_Solutions/Tut30Days/D27Testing.py
@@ -1,27 +1,94 @@
 # Your company needs a function that meets the following requirements:
 
-# For a given array of  integers, the function returns the index of the element with the minimum value in the array. If there is more than one element with the minimum value, it returns the smallest one.
-# If an empty array is passed to the function, it raises an exception. A colleague has written this method. The implementation in Python is listed below. Implementations in other languages can be found in the code template.
-# def minimum_index(seq):
-#     if len(seq) == 0:
-#         raise ValueError("Cannot get the minimum value index from an empty sequence")
-#     min_idx = 0
-#     for i in range(1, len(seq)):
-#         if a[i] < a[min_idx]:
-#             min_idx = i
-#     return min_idx
-# A coworker has prepared functions that will perform the tests and validate return values. Finish the implementation of  classes to provide data and expected results for the tests.
+# For a given array of  integers, the function returns the index of the element 
+# with the minimum value in the array. If there is more than one element with 
+# the minimum value, it returns the smallest one.
+# If an empty array is passed to the function, it raises an exception. A colleague 
+# has written this method. The implementation in Python is listed below. 
+def minimum_index(seq):
+    if len(seq) == 0:
+        raise ValueError("Cannot get the minimum value index from an empty sequence")
+    min_idx = 0
+    for i in range(1, len(seq)):
+        if seq[i] < seq[min_idx]:
+            min_idx = i
+    return min_idx
+# A coworker has prepared functions that will perform the tests and validate return 
+# values. Finish the implementation of  classes to provide data and expected results 
+# for the tests.
+
 
 # Complete the following methods.
 
 # In the class TestDataEmptyArray:
+class TestDataEmptyArray(object):
 
 # get_array() returns an empty array
+    @staticmethod
+    def get_array():
+        return []
+
+
 # In the class TestDataUniqueValues:
+class TestDataUniqueValues(object):
 
 # get_array() returns an array of size at least 2 with all unique elements
+    @staticmethod
+    def get_array():
+        return [1, 2, 3, 4, 5]
+
 # get_expected_result() returns the expected minimum value index for this array
+    @staticmethod
+    def get_expected_result():
+        return 0
+    
+
 # In the class TestDataExactlyTwoDifferentMinimums:
+class TestDataExactlyTwoDifferentMinimums(object):
 
 # get_array() returns an array where the minimum value occurs at exactly 2 indices
-# get_expected_result() returns the expected index
+    @staticmethod
+    def get_array():
+        return [1, 2, 3, 4, 5, 1]
+
+# get_expected_result() returns the expected index of the minimum value for this array
+    @staticmethod
+    def get_expected_result():
+        return 0
+    
+
+def TestWithEmptyArray():
+    try:
+        seq = TestDataEmptyArray.get_array()
+        result = minimum_index(seq)
+    except ValueError as e:
+        pass
+    else:
+        assert False
+
+
+def TestWithUniqueValues():
+    seq = TestDataUniqueValues.get_array()
+    assert len(seq) >= 2
+
+    assert len(list(set(seq))) == len(seq)
+
+    expected_result = TestDataUniqueValues.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+
+def TestiWithExactyTwoDifferentMinimums():
+    seq = TestDataExactlyTwoDifferentMinimums.get_array()
+    assert len(seq) >= 2
+    tmp = sorted(seq)
+    assert tmp[0] == tmp[1] and (len(tmp) == 2 or tmp[1] < tmp[2])
+
+    expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result()
+    result = minimum_index(seq)
+    assert result == expected_result
+
+TestWithEmptyArray()
+TestWithUniqueValues()
+TestiWithExactyTwoDifferentMinimums()
+print("OK")
