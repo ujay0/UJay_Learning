ae561fc7ae072f685e88f27540061a8c89de101a
UJay  <uutkarsh2@yahoo.com>
2026-05-26 23:46:53 +0530

    Problam Statment D28 implementation names and email addresses from a simulated Emails table

---
+++ HR_Solutions/Tut30Days/D28RegEx.py
@@ -0,0 +1,22 @@
+# Consider a database table, Emails, which has the attributes First Name 
+# and Email ID. Given N rows of data simulating the Emails table, print 
+# an alphabetically-ordered list of people whose email address ends in '@gmail.com'.
+
+
+# Input Format
+
+# The first line contains an integer, N, denoting the number of rows in the table.
+# Each of the N subsequent lines contains 2 space-separated values: a first 
+# name and an email ID.
+
+
+# Constraints
+# 2 <= N <= 30
+# Each of the first names consists of lowercase letters only.
+# Each of the email IDs consists of lowercase letters, @ and . only.
+# The length of the first name not to exceed 20 and email ID not to exceed 50.
+
+# Output Format
+# Print an alphabetically-ordered list of first names for every user with a
+# gmail.com email address. Each name must be printed on a new line.
+
