d4cbd373ccfedb936fb46c874da09529f7dacb39
UJay  <uutkarsh2@yahoo.com>
2026-05-28 01:30:10 +0530

    Problem statment day 29 for bitwise AND function for sets

---
+++ HR_Solutions/Tut30Days/D29BitwiseAND.py
@@ -0,0 +1,12 @@
+# !/bin/python3
+
+# Given sets S = {1, 2, 3, ..., N} and an integer K, find the maximum value 
+# of A & B (where & represents the bitwise AND operator) such that A and B 
+# are different members of set S and the value of A & B is less than K.
+# 
+# 
+# Complete the 'bitwiseAnd' function below.
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
