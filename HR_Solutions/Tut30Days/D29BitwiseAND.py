9e5c4ce27585a5d259dc79fd081589164a6ddd54
UJay  <uutkarsh2@yahoo.com>
2026-05-28 23:32:14 +0530

    Refactor bitwiseAnd function and enhance comments for clarity

--- HR_Solutions/Tut30Days/D29BitwiseAND.py
+++ HR_Solutions/Tut30Days/D29BitwiseAND.py
@@ -14,9 +14,29 @@
 
 
 
-def bitwiseAnd(N, K):
-    # Write your code here
+# For example, when N = 5 and K = 2, the possible values of A & B are:
+#
+# 1 & 2 = 0
+# 1 & 3 = 1
+# 1 & 4 = 0
+# 1 & 5 = 1
+# 2 & 3 = 2
+# 2 & 4 = 0
+# 2 & 5 = 0
+# 3 & 4 = 0
+# 3 & 5 = 1
+# 4 & 5 = 4
+# The maximum value of A & B that is less than K = 2 is 1.
 
+def bitwiseAnd(N, K):
+    max_value = 0
+    for A in range(1, N + 1):
+        for B in range(A + 1, N + 1):
+            current_value = A & B
+            if max_value < current_value < K:
+                max_value = current_value
+            # bitwiseAnd_UJ(A, B)
+    return max_value
 
 
 if __name__ == '__main__':
