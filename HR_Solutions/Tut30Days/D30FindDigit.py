dfd63ee56d2f4cb026a154fe765b78a706a5aaf5
UJay  <uutkarsh2@yahoo.com>
2026-05-29 15:54:50 +0530

    Problem statment for Day 30 of findDigits function

---
+++ HR_Solutions/Tut30Days/D30FindDigit.py
@@ -0,0 +1,10 @@
+# An integer d is a divisor of an integer n if the remainder 
+# of n/d = 0. Given an integer, for each digit that makes
+#  up the integer determine whether it is a divisor. 
+# Count the number of divisors occurring within the integer.
+# For example, if n = 1248, the digits are 1, 2, 4, and 8.
+# 1, 2, and 4 are divisors of 1248, but 8 is not. Return the number of divisors.
+
+# Complete the 'findDigits' function below.
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
