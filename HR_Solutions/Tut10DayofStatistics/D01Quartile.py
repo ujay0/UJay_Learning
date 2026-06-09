65fbdf4b7d6ceb136a81db452e230fecbd0e09f0
UJay  <uutkarsh2@yahoo.com>
2026-06-09 11:57:53 +0530

    Implement quartiles function to calculate Q1, Q2, and Q3 from input array

--- HR_Solutions/Tut10DayofStatistics/D01Quartile.py
+++ HR_Solutions/Tut10DayofStatistics/D01Quartile.py
@@ -26,12 +26,32 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
+def median(data):
+        n = len(data)
+        if n % 2 == 0:
+            med = (data[n // 2 - 1] + data[n // 2]) / 2
+            return int(med)
+        else:
+            return int(data[n // 2])
+
 def quartiles(arr):
     # Write your code here
-    pass
+    arr.sort()
+    
+    n = len(arr)
+
+    q2 = median(arr)
+    if n % 2 == 0:
+        q1 = median(arr[:n // 2])
+        q3 = median(arr[n // 2:])
+    else:
+        q1 = median(arr[:n // 2])
+        q3 = median(arr[n // 2 + 1:])
+    # print(q1, q2, q3)
+    return [q1, q2, q3]
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input().strip())
 
@@ -39,7 +59,7 @@ if __name__ == '__main__':
 
     res = quartiles(data)
 
-    fptr.write('\n'.join(map(str, res)))
-    fptr.write('\n')
+    print('\n'.join(map(str, res)))
+    # fptr.write('\n')
 
-    fptr.close()
+    # fptr.close()
