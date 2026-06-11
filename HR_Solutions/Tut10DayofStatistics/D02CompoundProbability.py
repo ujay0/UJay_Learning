30a46075942f7922216e59ea8566d94aad8001a9
UJay  <uutkarsh2@yahoo.com>
2026-06-11 21:11:08 +0530

    Problem Statment for Compound Probability calculations

---
+++ HR_Solutions/Tut10DayofStatistics/D02CompoundProbability.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+
+
+# Task
+# There are 3 urns labeled X, Y, and Z.
+
+
+# Urn X contains 4 red balls and 3 black balls.
+# Urn Y contains 5 red balls and 4 black balls.
+# Urn Z contains 1 red ball and 1 black ball.
+
+# One ball is drawn from each of the  urns. What is the probability that, of the  balls drawn,  are red and  is black?
