19ab7e34813347740607f22170d6cac9e3e13a64
UJay  <uutkarsh2@yahoo.com>
2026-07-14 15:22:23 +0530

    Enhance documentation for Central Limit Theorem implementation, clarifying input and output formats

--- HR_Solutions/Tut10DayofStatistics/D06TheCentralLimitTheoremIII.py
+++ HR_Solutions/Tut10DayofStatistics/D06TheCentralLimitTheoremIII.py
@@ -1,6 +1,30 @@
 #!/bin/python3
 
-# Task
-# You have a sample of  values from numpy import mean
 
-# from a population with mean  and with standard deviation . Compute the interval that covers the middle  of the distribution of the sample mean; in other words, compute  and  such that . Use the value of . Note that  is the z-score. 
+# 
+# Objective
+# Task
+# You have a sample of 100 values from a population with mean u=500 and with standard deviation o=80.
+# Compute the interval that covers the middle 95% of the distribution of the sample mean; 
+# in other words, compute A and B such that P(A < X̄ < B) = 0.95. Use the value of z₀.₀₂₅ = 1.96. 
+# Note that X̄ is the sample mean.
+
+# Input Format
+
+# There are five lines of input (shown below):
+# 100
+# 500
+# 80
+# .95
+# 1.96
+# The first line contains the sample size. The second and third lines contain the respective mean (u) 
+# and standard deviation (o). The fourth line contains the distribution percentage we want to cover 
+# (as a decimal), and the fifth line contains the value of z.
+
+
+# Output Format
+# Print the following two lines of output, rounded to a scale of 2 decimal places (i.e., 1.23  format):
+
+# On the first line, print the value of A.
+# On the second line, print the value of B.
+
