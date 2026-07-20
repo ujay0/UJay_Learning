b032e8e62206ac6c35f7c37b10e57ed87158dab9
UJay  <uutkarsh2@yahoo.com>
2026-07-21 00:48:09 +0530

    Problem Statment for least squares regression line calculation

---
+++ HR_Solutions/Tut10DayofStatistics/D08LeastSquareRegressionLine.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+# Task
+# A group of five students enrolls in Statistics immediately after taking a Math 
+# aptitude test. Each student's Math aptitude test score, x, and Statistics course 
+# grade, y, can be expressed as the following list of (x, y) points:
+# (95, 85), 
+# (85, 95), 
+# (80, 70), 
+# (70, 65), 
+# (60, 70)
+
+# If a student scored an 80 on the Math aptitude test, what grade would we expect 
+# them to achieve in Statistics? Determine the equation of the best-fit line 
+# using the least squares method, then compute and print the value of x when y=80.
+
+# Input Format
+
+# There are five lines of input; each line contains two space-separated integers
+#  describing a student's respective x and y grades:
+# 95 85
+# 85 95
+# 80 70
+# 70 65
+# 60 70
+# Output Format
+# Print a single line denoting the answer, rounded to a scale of 3 decimal
+#  places (i.e., 1.000 format).
